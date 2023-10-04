6d3f8ebe6e88bdb0331874fd04093d8b9dcd89c2
unknown <calemamanispe.12@gmail.com>
2023-10-04 09:24:43 -0400

    lab3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
-  return (
-    <main className="flex flexexit flex-col items-center justify-between p-24">
-      Main Data
-    </main>
-  )
+import fetchPost from "@/services/blog/fetchPost"
+import { Post } from "@/services/models/models"
+
+const Posts = async() => {
+    const post: Post[] = await fetchPost();
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Post</h1>
+            {post.map((post: Post) => (
+                <div className="bg-gray-200 p-4 rounded-lg shadow-lg">
+                    <h2 className="text-xl font-semibold">{post.title}</h2>
+                    <p>{post.body}</p>
+                </div>
+            )
+            )}
+        </div>
+    )
 }
+export default Posts
